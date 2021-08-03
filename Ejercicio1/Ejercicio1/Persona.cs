636844103c6cff0fdbd8af1f5b556131674c1569
Cano Marcelo <mecano@catamarca.edu.ar>
2021-08-02 22:13:47 -0300

    Ejericio 1 del trabajo practico 6

---
+++ Ejercicio1/Ejercicio1/Persona.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Persona
+    {
+        private string nombre;
+        private string dni;
+        private byte edad;
+
+
+        public Persona()
+        {
+            nombre = string.Empty;
+            dni = "";
+            edad = 0;
+ 
+        }
+        public Persona(string nom, string documento, byte anios)
+        {
+            this.nombre = nom;
+            this.dni = documento;
+            this.edad = anios;
+        }
+
+        //Métodos getter y Setter
+        public void setNombre(string nom)
+        {
+            if (string.IsNullOrEmpty(nom))
+            {
+                Console.WriteLine("Debe ingresar un nombre");
+                return;
+            }
+
+            this.nombre = nom;
+        }
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setDNI(string doc)
+        {
+            if (string.IsNullOrEmpty(doc))
+            {
+                Console.WriteLine("Debe ingresar el dni");
+                return;
+            }
+
+            this.dni = doc;
+ 
+        }
+        public string getDNI()
+        {
+            return dni;
+        }
+
+        public void setEdad(byte anio)
+        {
+            if (anio < 0)
+            {
+                Console.WriteLine("La edad No puede ser negativa");
+                return;
+            }
+
+            this.edad = anio;
+        }
+
+        public byte getEdad()
+        {
+            return edad;
+        }
+
+
+        public void Mostrar()
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Nombre: " + getNombre());
+            Console.WriteLine("DNI: {0}", getDNI());
+            Console.WriteLine("Edad: " + getEdad());
+            Console.WriteLine("-------------------");
+ 
+        }
+
+
+        public bool esMayorDeEdad()
+        {
+            if (getEdad() >= 18)
+                return true;
+            else
+                return false;
+        }
+
+
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona alumno1 = new Persona();
+
+            Persona alumno2 = new Persona("Daniel","3025663",10);
+
+            Console.WriteLine("Ingresar el nombre");
+            string apodo = Console.ReadLine();
+            alumno1.setNombre(apodo);
+
+            Console.WriteLine("Ingrese el dni: ");
+            alumno1.setDNI(Console.ReadLine());
+
+            Console.WriteLine("Ingrese una edad (mayor a cero) : ");
+            alumno1.setEdad(byte.Parse(Console.ReadLine()));
+
+
+            alumno1.Mostrar();
+            alumno2.Mostrar();
+
+            if (alumno1.esMayorDeEdad())
+                Console.WriteLine("{0} es mayor de edad", alumno1.getNombre());
+            else
+                Console.WriteLine("{0} es menor de edad", alumno1.getNombre());
+
+            /*  condicion ? 'valor si es verdadero' : 'valor si es falso'   */
+
+            /*  a>b => true/false  (a<b)&&(c!=0) => true/false  */
+
+            Console.WriteLine("{0} es {1} de edad" ,alumno2.getNombre(), alumno2.esMayorDeEdad() ? "mayor" : "menor");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
